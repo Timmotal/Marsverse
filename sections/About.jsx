ea28354ca9e384157bbc95e2654788fa3b3486c1
move style to head <timmortal44@yahoo.com>
2023-03-02 02:30:40 +0100

    added the texts casuing deployments failure and some couple of changes

--- sections/About.jsx
+++ sections/About.jsx
@@ -25,18 +25,18 @@ const About = () => (
         variants={fadeIn('up', 'tween', .2, 1)}
         className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
       >
-         <span className="font-extrabold text-white">Metaverse </span>
-         is a new thing in the future, where you can enjoy the virtual 
-         world by feeling like it's really real, you can feel what you 
-         feel in this metaverse world, because this is really the 
-         <span className="font-extrabold text-white"> madness of the metaverse </span> 
+         <span className="font-extrabold text-white">Marsverse </span>
+         is a new home in the future, where you can enjoy living on mars
+          by feeling like it's really real, you can feel what you 
+         feel in this marsverse world, because this is really the 
+         <span className="font-extrabold text-white"> madness of the marsverse </span> 
          of today, using only 
          <span className="font-extrabold text-white"> VR </span>
-         devices you can easily explore the metaverse
+         devices you can easily explore the marsverse
          world you want, turn your dreams into reality. Let's 
          <span className="font-extrabold text-white"> explore </span>
          the 
-         <span className="font-extrabold text-white"> madness of the metaverse </span> 
+         <span className="font-extrabold text-white"> madness of the marsverse </span> 
          by scrolling down
       </motion.p>
 
