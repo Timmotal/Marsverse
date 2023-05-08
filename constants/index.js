61eeec8c0ac2a869b3fb938f49cf68ac202b3f69
move style to head <timmortal44@yahoo.com>
2023-05-08 23:17:07 +0100

    update the insight card 01 title

--- constants/index.js
+++ constants/index.js
@@ -50,7 +50,7 @@ export const newFeatures = [
 export const insights = [
   {
     imgUrl: '/planet-06.png',
-    title: 'The launch of the Metaverse makes Elon musk ketar-ketir',
+    title: 'The launch of the Metaverse makes Elon musk the Sultan of the ZANGER',
     subtitle:
         'Magna etiam tempor orci eu lobortis elementum nibh tellus molestie. Diam maecenas sed enim ut sem viverra alique.',
   },
@@ -59,7 +59,7 @@ export const insights = [
     title: '7 tips to easily master the madness of the Metaverse',
     subtitle:
         'Vitae congue eu consequat ac felis donec. Et magnis dis parturient montes nascetur ridiculus mus. Convallis tellus id interdum',
-  },
+  }, 
   {
     imgUrl: '/planet-08.png',
     title: 'With one platform you can explore the whole world virtually',
